c611f6c24647ee64ca851894f84ebf8c5b41de07
Muhammad Huzaifa <huzaifa2493@gmail.com>
2024-11-10 18:37:56 +0500

    Npm erorr solved

--- app/Components/resume.tsx
+++ app/Components/resume.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";  // <-- Correct imports
 
 export default function Resume() {
